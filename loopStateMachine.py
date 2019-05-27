6be5e627842d7e376974756efa04242350dd5f7e
alexmickelson <alexmickelson96@gmail.com>
2019-05-27 11:38:10 -0600

    gitignore

--- loopStateMachine.py
+++ loopStateMachine.py
@@ -9,6 +9,11 @@ class loopStateMachine:
         self.midi=midi.loop(loopNumber)
 
         self.blankState=states.blankState(self)
+        self.recState=states.recState(self)
+        self.playState=states.playState(self)
+        self.pauseState=states.pauseState(self)
+        self.dubState=states.dubState(self)
+        
         self.currentState = self.blankState
     
     def pressPedal1(self):
